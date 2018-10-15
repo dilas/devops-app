6d30bb7fffc9b8dc3008300fd9adeda4f0c0549d
Anderson Lobo Feitosa <andersonlf@gmail.com>
2018-10-15 19:21:49 -0300

    removendo nome da empresa e criacao de 2 bombs

--- src/main/java/br/com/devops/controller/ConnectionController.java
+++ src/main/java/br/com/devops/controller/ConnectionController.java
@@ -1,13 +1,15 @@
-package br.com.cabal.devops.controller;
+package br.com.devops.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 @RestController("/connection")
 public class ConnectionController {
 
     @GetMapping("/start")
     public void start() {
-        // TODO implementation
     }
 }
